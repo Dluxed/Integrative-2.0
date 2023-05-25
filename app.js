f3478d9f375016ea18a29e17eac7afa6201a208a
DerekMZ <richyrichon0@gmail.com>
2023-05-25 10:35:07 -0700

    First commit DTM

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express') //requerimos el modulo de express
+const app = express() //creando obj
+const port = 3000
+const path = require("path")
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/docs', (req, res) => {
+  res.sendFile(path.join(__dirname + '/limon.html'))
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
