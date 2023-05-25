7a40689efeb9498e94dd9c2bb0ad182955ec6d21
DerekMZ <richyrichon0@gmail.com>
2023-05-25 12:19:49 -0700

    change to app.js

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ const app = express() //creando obj
 const port = 3000
 const path = require("path")
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
-app.get('/docs', (req, res) => {
+
+
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/home.html'))
 })
 
