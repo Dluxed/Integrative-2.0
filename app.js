d805e0c186cdc2567e0ce61b6d2797359c96ace2
Vented5 <venteddiagram@gmail.com>
2023-06-02 10:24:21 -0600

    ya quedo el tailwind, usar el comando abreviado para correr

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ const app = express() //creando obj
 const port = 3000
 const path = require("path")
 
-
+app.use(express.static(path.join(__dirname, 'src')));
 
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/home.html'))
+  res.sendFile(path.join(__dirname + '/src/templates/home.html'))
 })
 
+app.get('/hi', (req, res) => {
+  res.sendFile(path.join(__dirname + '/src/templates/hola.html'))
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
