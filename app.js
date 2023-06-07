29ab4e8151a565b6f39fc12eec7f07f5ef1c84f3
DerekMZ <richyrichon0@gmail.com>
2023-06-07 12:43:56 -0700

    Mock-up 1.1

--- app.js
+++ app.js
@@ -14,6 +14,14 @@ app.get('/hi', (req, res) => {
   res.sendFile(path.join(__dirname + '/src/templates/hola.html'))
 })
 
+app.get('/log', (req, res) => {
+  res.sendFile(path.join(__dirname + '/src/templates/login.html'))
+})
+
+app.get('/reg', (req, res) => {
+  res.sendFile(path.join(__dirname + '/src/templates/register.html'))
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const mongoose  = require('mongoose');
 const app = express();
 const port = 3000;
+const {MongoClient} = require("mongodb");
+const dbName = "DrStone";
+require ('dotenv').config()
 
-//const url = "";//
+const url = process.env.URI_MONGO;
 
 async function connect(){
     try {
@@ -14,4 +17,30 @@ async function connect(){
     }
 }
 
+const client = new MongoClient(url);
+
 connect();
+
+async function insert(){
+    try {
+        await client.connect();
+        console.log("hola patron");
+        const db = client.db(dbName);
+        const col = db.collection("Personajes");
+
+        let suika = {
+            "name" : "Chrome",
+            "sexo": "hombre"
+        }
+
+        const p = await col.insertOne(suika);
+        const myDoc = await col.findOne();
+    } 
+    catch(err){
+        console.log(err.stack);
+    }
+    finally{
+        await client.close();
+    }
+}
+insert();
