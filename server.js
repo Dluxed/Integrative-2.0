e245078860554e3af9d8138c9d528f78fbdc3e1b
Vented5 <venteddiagram@gmail.com>
2023-06-08 09:40:44 -0600

    Dotenv library added

--- server.js
+++ server.js
@@ -1,17 +1,54 @@
-const express = require('express');
 const mongoose  = require('mongoose');
-const app = express();
-const port = 3000;
+const {MongoClient} = require("mongodb");
 
-//const url = "";//
+require('dotenv').config()
+const url = process.env.URI_MONGO 
+const client = new MongoClient(url);
+const dbName = "DrStone";
 
 async function connect(){
     try {
         await mongoose.connect(url)
-        console.log("Vive debajo de una piña del mar")
+        console.log("Ahueso tenemos coneccion (☞ﾟヮﾟ)☞")
     } catch (error) {
-        console.log("Valio we " + error)
+        console.log("Valio we ");
+        console.log(error);
+        
     }
 }
 
 connect();
+
+async function insert(){
+    try {
+        await client.connect();
+        const db = client.db(dbName);
+        const col = db.collection("Personajes");
+
+        let suika = {
+            "name" : "Chrome",
+            "sexo": "hombre"
+        }
+
+        const p = await col.insertOne(suika);
+        const myDoc = await col.findOne();
+    } 
+    catch(err){
+        console.log(err.stack);
+    }
+    finally{
+        await client.close();
+    }
+}
+//insert();
+
+async function select(){
+    await client.connect();   
+    const db = client.db(dbName);
+    const col = db.collection("Personajes");
+    const myDoc = await col.findOne();
+    
+    console.log(myDoc);
+}
+
+select();
