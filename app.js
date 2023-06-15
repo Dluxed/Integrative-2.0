3ab20a8699e040d481a0d65df6fdad27058ab87e
Vented <venteddiagram@gmail.com>
2023-06-15 04:03:23 -0500

    metodo post agregado al login

--- app.js
+++ app.js
@@ -3,23 +3,33 @@ const express = require('express')
 const app = express() //creando obj
 const port = 3000
 
-const path = require("path");//El path de toda la vida
+//El path de toda la vida
+const path = require("path");
 
-const engine = require('ejs-mate');//Modulacion de vistas ejs
-
+//Modulacion de vistas ejs
+const engine = require('ejs-mate');
 app.engine('ejs', engine);
 app.set('views', __dirname + '/src/templates');
 
-
+//Archivos estaticos
 app.use(express.static(path.join(__dirname, 'src')));
 
-//Redirecciones del servidor
+//Desmadre de POST
+app.use(express.json());
+app.use(express.urlencoded());
+
+//-----------   Redirecciones del servidor  -------------------
 app.get('/', (req, res) => {
   res.render('home.ejs')
 })
 
 app.get('/log', (req, res) => {
   res.render('login.ejs')
+})
+
+app.post('/logPost', (req, res) => {
+  console.log(req.body);
+  console.log(req.body['usr']);
 })
 
 app.get('/reg', (req, res) => {
