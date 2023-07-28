2d50920ec0e2211aba255959853e4264db78bc3e
Vented <venteddiagram@gmail.com>
2023-07-28 01:43:41 -0500

    bugs fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,12 @@ module.exports = {
   content: ['./**/*.html', './**/*.js', './**/*.ejs'],
   theme: {
     extend: {
+      colors:{
+        'primary': "#0EB29A",
+        'secondary': "#F5FDFF",
+        'background': "#8C999A",
+        'item': "#DDF0C2"
+      },
       backgroundImage:{
         'dogpark': "url('/src/images/dogbg.jpg')"
       }
